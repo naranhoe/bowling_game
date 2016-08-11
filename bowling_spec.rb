e7c7cfe4e29f4f3d09a498fce91fb331235b4cf9
Jorge Naranjo <j.naranjojr@gmail.com>
2016-08-11 14:10:13 -0400

    All 5 examples are passing. Bowling game is working efficiently.

--- bowling_spec.rb
+++ bowling_spec.rb
@@ -3,14 +3,39 @@ require './bowling'
 
 describe 'Bowling game logic' do
 
-  it 'Can make a new game' do
-    game = Bowling.new
+  before do
+    @game = Bowling.new
   end
 
   it 'Can roll gutter game' do
-    game = Bowling.new
-    20.times{ game.roll(0) }
-    expect(game.score).to eq(0)
+    20.times{ @game.roll(0) }
+    expect(@game.score).to eq(0)
+  end
+
+  it 'Can roll all ones' do
+    20.times{ @game.roll(1) }
+    expect(@game.score).to eq(20)
+  end
+
+  it 'Can roll a spare' do
+    @game.roll(5)
+    @game.roll(5)
+    @game.roll(3)
+    17.times{ @game.roll(0) }
+    expect(@game.score).to eq(16)
+  end
+
+  it 'Can roll a strike' do
+    @game.roll(10)
+    @game.roll(3)
+    @game.roll(4)
+    16.times{ @game.roll(0) }
+    expect(@game.score).to eq(24)
+  end
+
+  it 'Can roll a perfect game' do
+    12.times{ @game.roll(10) }
+    expect(@game.score).to eq(300)
   end
 
 end
